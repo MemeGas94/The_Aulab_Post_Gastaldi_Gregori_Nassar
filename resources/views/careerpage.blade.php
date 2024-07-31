f6bab24438bdac883ea720126f6ad2c228299d00
GabrieleGregori <gabriel90_rd@hotmail.com>
2024-07-31 18:39:19 +0200

    ultime modifiche

--- resources/views/careerpage.blade.php
+++ resources/views/careerpage.blade.php
@@ -4,7 +4,7 @@
         @csrf
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" disabled aria-describedby="emailHelp" name="email">
+          <input type="email" class="form-control" id="exampleInputEmail1" value="{{Auth::user()->email}}" disabled aria-describedby="emailHelp" name="email">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Messaggio di presentazione</label>

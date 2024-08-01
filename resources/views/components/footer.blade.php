9558af4e17b375378186fbe651766fdb41994b3a
Dauria Mattia <mattiad606@gmail.com>
2024-08-01 04:12:01 +0200

    navbar-footer sfondo e altri ritocchi di stile

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,32 @@
-<div class="vh-80">
+
+<footer>
+    <div class="footer">
+    <div class="row ">
+    <a href="#"><i class="fa fa-facebook"></i></a>
+    <a href="#"><i class="fa fa-instagram"></i></a>
+    <a href="#"><i class="fa fa-youtube"></i></a>
+    <a href="#"><i class="fa fa-twitter"></i></a>
+    </div>
+    
+    <div class="row">
+    <ul>
+    <li><a href="#">Contact us</a></li>
+    <li><a href="#">Our Services</a></li>
+    <li><a href="#">Privacy Policy</a></li>
+    <li><a href="#">Terms & Conditions</a></li>
+    <li><a href="#">Career</a></li>
+    </ul>
+    </div>
+    
+    <div class="row mx-5">
+    The Aulab Post © 2024  
+    </div>
+    </div>
+    </footer>
+    
+
+
+
+{{-- <div class="vh-80">
     <h2 class="text-center">io sono il footer</h2>
-</div>
+</div> --}}
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!--FONT AWESOME-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Play&display=swap" rel="stylesheet"> 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>

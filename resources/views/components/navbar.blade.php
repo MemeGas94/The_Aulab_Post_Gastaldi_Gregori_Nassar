77dec769bcf25932da693a93c3e39ca08e041af0
Samuele Gastaldi <gastaldi.meme@gmail.com>
2024-08-05 15:43:44 +0200

    aggiunta sezione video

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+      <div class="collapse navbar-collapse " id="navbarNav">
+        <ul class="navbar-nav bg-danger ">
           <li class="nav-item">
             <a class="nav-link active text-white" aria-current="page" href="{{route('homepage')}}">Home</a>
           </li>
@@ -21,49 +21,67 @@
             </ul>
           </li> 
 
-         @guest
-          <li class="nav-item">
-            <a class="nav-link text-white" href="{{route('register')}}">Register</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link text-white" href="{{route('login')}}">Login</a>
-          </li>
-          @endguest
-
+          
           @auth
           <li class="nav-item">
             <a class="nav-link text-white" href="{{route('Article.create')}}">inserisci articolo</a>
           </li>
-
+          
           <li class="nav-item">
             <a class="nav-link text-white" href="#" onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Logout</a>
           </li>
-
+          
           <li class="nav-item">
             <a class="nav-link text-white" href="{{route('careerpage')}}">Lavora con noi</a>
           </li>
-
+          
           <li class="nav-item">
             <a class="nav-link text-white" href="{{route('admin.dashboard')}}">Richieste</a>
           </li>
-
+          
           <li class="nav-item">
             <a class="nav-link text-white" href="{{route('revisor.dashboard')}}">Revisione articoli</a>
           </li>
-
-
+          
+          
           <form method="POST" id="form-logout" action="{{route('logout')}}">
-
+            
             @csrf
             
             
             
-           
+            
           </form>
-    
-              
+          
+          
           @endauth
+          
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          
+          @guest
+          
+           <li class="nav-item">
+             <a class="nav-link text-white" href="{{route('register')}}">Register</a>
+           </li>
+  
+           <li class="nav-item">
+             <a class="nav-link text-white" href="{{route('login')}}">Login</a>
+           </li>
+           @endguest
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         </ul>
       </div>

62ea1efe8a7f62c4892b8a86a8070d7781afab39
GabrieleGregori <gabriel90_rd@hotmail.com>
2024-08-06 11:53:01 +0200

    inserito articolo storia delle olimpiadi

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,27 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <!--FONT AWESOME-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Play&display=swap" rel="stylesheet"> 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <!--FONT AWESOME-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Play&display=swap" rel="stylesheet"> 
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
 <body>
 
     <x-navbar/>
     <x-header/>
    
     <div class="min-vh-100">
-
-    {{$slot}}
-
-
-   </div>
+        {{$slot}}
+    </div>
 
     <x-footer/>
     
